13bb909512fba8e02019c93a6bfeebf41b9bd80d
Ataullah Mesbah Fahim <mesbahataullah4@gmail.comm>
2023-06-10 05:46:39 +0600

    Login and sign up page navbar and footer remove

--- src/hooks/useClasses.jsx
+++ src/hooks/useClasses.jsx
@@ -8,7 +8,7 @@ const useClasses = () => {
     const [loading, setLoading] = useState(true);
 
     useEffect(() => {
-      fetch('classes.json')
+      fetch('http://localhost:5000/classes')
         .then(res => res.json())
         .then(data => {
           setClasses(data);
