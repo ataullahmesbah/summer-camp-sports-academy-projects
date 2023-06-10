8b1e5715861adb9cc99ab57e2d7d6aa50dbbfb43
Ataullah Mesbah Fahim <mesbahataullah4@gmail.comm>
2023-06-11 04:43:00 +0600

    payment section add

--- src/Layout/Dashboard.jsx
+++ src/Layout/Dashboard.jsx
@@ -1,6 +1,7 @@
 import { NavLink, Outlet } from "react-router-dom";
 import { FaAlignLeft } from "react-icons/fa";
 import { FaShoppingCart, FaBookOpen, FaDollarSign, FaUserGraduate, FaHome, FaPeopleArrows } from "react-icons/fa";
+import icon from '../Pages/Images/Education/learning.png'
 
 
 const Dashboard = () => {
@@ -24,6 +25,11 @@ const Dashboard = () => {
 
 
                     {/* Sidebar content here */}
+
+                    <div className="mx-4 mt-3 flex gap-3">
+                        <img className="w-8 h-8" src={icon} alt="" />
+                    <li className="text-3xl  mb-8 text-yellow-500 font-bold">Elite Athlete <br /> Arena</li>
+                    </div>
 
                     <li><NavLink to='/dashboard/myclasses' className='font-bold text-xl text-white'><FaUserGraduate /> Student Home</NavLink></li>
                     <li><NavLink to='/dashboard/myclasses' className='font-semibold text-xl text-white'><FaBookOpen /> Enroll Class</NavLink></li>
--- src/Pages/Dashboard/MyClasses/MyClasses.jsx
+++ src/Pages/Dashboard/MyClasses/MyClasses.jsx
@@ -1,4 +1,5 @@
 
+import { FaShoppingCart } from "react-icons/fa";
 import useBookingClass from "../../../hooks/useBookingClass";
 import Container from "../../Shared/Container";
 import Swal from "sweetalert2";
@@ -18,33 +19,51 @@ const MyClasses = () => {
             cancelButtonColor: '#d33',
             confirmButtonText: 'Yes, delete it!'
         })
-        .then((result) => {
-            if(result.isConfirmed){
-                fetch(`http://localhost:5000/bookingClass/${booking._id}`,{
-                    method: 'DELETE'
-                })
-                .then(res => res.json())
-                .then(data => {
-                    if(data.deletedCount > 0){
-                        refetch();
-                        Swal.fire(
-                            'Deleted!',
-                            'Your Booking has been deleted.',
-                            'success'
-                        )
-                    }
-                })
-            }
-        })
+            .then((result) => {
+                if (result.isConfirmed) {
+                    fetch(`http://localhost:5000/bookingClass/${booking._id}`, {
+                        method: 'DELETE'
+                    })
+                        .then(res => res.json())
+                        .then(data => {
+                            if (data.deletedCount > 0) {
+                                refetch();
+                                Swal.fire(
+                                    'Deleted!',
+                                    'Your Booking has been deleted.',
+                                    'success'
+                                )
+                            }
+                        })
+                }
+            })
     }
 
     return (
         <Container>
             <div className="">
-                <div className="flex space-x-10 mb-10">
-                    <h1 className="text-center">Total items: {bookingClass.length}</h1>
-                    <h1 className="text-center">Total Price: ${total}</h1>
+                <div className="text-center">
+                <h1 className="font-bold text-4xl p-5"><span className="text-blue-950">Empowering Students:</span> <span className="text-gray-600">
+                Book Your Classes and <br /> Summer Camp Learning Experience</span></h1>
+                <p className="mb-10">Welcome to our student-centered platform, where we put the power in the hands of learners. <br /> With our user-friendly interface, students can now easily book their desired classes <br /> and summer camp learning experiences. Whether you are looking to enhance <br /> your academic skills</p>
                 </div>
+                
+                <div className="mb-5 flex items-center justify-between">
+                    <h1 className=" flex items-center">
+                        <span className="font-semibold text-xl mx-5">Total Bookings:</span>
+                        <button className="relative flex items-center gap-2">
+                            <FaShoppingCart className="bg-white text-black rounded-full p-1 text-4xl" />
+                            <div className="absolute -top-2 -right-2 bg-purple-900 text-white rounded-full w-5 h-5 flex items-center justify-center">
+                                {bookingClass?.length || 0}
+                            </div>
+                        </button>
+                    </h1>
+                    <div className="space-y-2">
+                    <h1 className="font-semibold text-xl">Sub Total: <span className="text-blue-950 text-2xl">${total}</span></h1>
+                    <button className="btn-sm btn-outline btn-secondary btn mx-16">Payment</button>
+                    </div>
+                </div>
+                
                 <div className="overflow-x-auto">
                     <table className="table">
                         {/* head */}
