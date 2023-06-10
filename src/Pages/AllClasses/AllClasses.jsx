a188ad125d1cda4ba3f7ca17079ec0943b4d813c
Ataullah Mesbah Fahim <mesbahataullah4@gmail.comm>
2023-06-10 06:20:05 +0600

    student selected class and booking private route

--- src/Pages/AllClasses/AllClasses.jsx
+++ src/Pages/AllClasses/AllClasses.jsx
@@ -1,6 +1,46 @@
+import { useContext } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import { AuthContext } from "../../Providers/AuthProvider";
+import Swal from "sweetalert2";
 
 const AllClasses = ({ allClass }) => {
     const { image, name, instructor_name, available_seat, price, } = allClass;
+    const { user } = useContext(AuthContext);
+    const navigate = useNavigate();
+    const location = useLocation(); 
+
+    const handleBookingClass = allClass => {
+        if(user){
+            fetch('http://localhost:5000/bookingClass')
+            .then(res => res.json())
+            .then(data => {
+                if(data.insertedId){
+                    Swal.fire({
+                        position: 'top-end',
+                        icon: 'success',
+                        title: 'Your work has been saved',
+                        showConfirmButton: false,
+                        timer: 1500
+                      })
+                }
+            })
+        }
+        else {
+            Swal.fire({
+                title: 'Log In Now, and Booking Your Favorite Class ',
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Login Now'
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    navigate('/login', { state: { from: location } })
+                }
+            })
+        }
+    }
+
     return (
 
         <div className="card w-96 bg-base-100 shadow-xl">
@@ -13,7 +53,7 @@ const AllClasses = ({ allClass }) => {
                 <p>{available_seat}</p>
                 <p>{price}</p>
                 <div className="card-actions justify-end">
-                    <button className="btn btn-primary">Enroll Now</button>
+                    <button onClick={() => handleBookingClass(allClass)} className="btn btn-primary">Book Now</button>
                 </div>
             </div>
         </div>
