9207a8aebc3ffbe1106a84515da1fae8b787df8a
Ataullah Mesbah Fahim <mesbahataullah4@gmail.comm>
2023-06-12 04:04:22 +0600

    useAxiosSecure implement

--- src/hooks/useBookingClass.jsx
+++ src/hooks/useBookingClass.jsx
@@ -1,16 +1,21 @@
 import { useQuery } from '@tanstack/react-query'
-
 import { useContext } from 'react';
 import { AuthContext } from '../Providers/AuthProvider';
 
+
 const useBookingClass = () => {
     const { user, loading } = useContext(AuthContext);
+    const token = localStorage.getItem('access-token')
 
     const { refetch, data: bookingClass = [] } = useQuery({
         queryKey: ['bookingClass', user?.email],
         enabled: loading,
         queryFn: async () => {
-            const res = await fetch(`http://localhost:5000/bookingClass?email=${user?.email}`)
+            const res = await fetch(`http://localhost:5000/bookingClass?email=${user?.email}`, {
+                headers: {
+                    authorization: `bearer ${token}`
+                }
+            })
             return res.json();
         }
     })
